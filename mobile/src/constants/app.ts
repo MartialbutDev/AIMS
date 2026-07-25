27da42b841b0474c4c04943444f4dd6fe1ea0ef7
Varren Meg Naive <varrenmegm.naive@gmail.com>
2026-07-25 20:47:10 +0800

    Trigger GitHub refresh for mobile folder

--- mobile/src/constants/app.ts
+++
@@ -1,3 +0,0 @@
-export const APP_NAME = "Academic Internship Management System";
-
-export const APP_VERSION = "Version 1.0.0";
--- mobile/src/theme/typography.ts
+++
@@ -1,9 +0,0 @@
-const Typography = {
-  title: 30,
-  heading: 24,
-  subheading: 18,
-  body: 16,
-  caption: 14,
-};
-
-export default Typography;
